443f7a67f8c1652cb7f04f98ff85cc7417c72d39
Isaad Khan <IKhan@lcs.on.ca>
2023-04-18 19:18:49 -0400

    Can now use a button to fetch another joke.

--- Jokes/Views/JokeView.swift
+++ Jokes/Views/JokeView.swift
@@ -13,40 +13,57 @@ struct JokeView: View {
     @State var punchlineOpacity = 0.0
     
     // The current joke to display
-    @State var currentJoke = exampleJoke
+    @State var currentJoke: Joke?
     
     // MARK: Computed properties
     var body: some View {
         NavigationView{
             VStack{
-                Text(currentJoke.setup)
-                    .font(.title)
-                    .multilineTextAlignment(.center)
                 
-                Button(action: {
-                    withAnimation(.easeIn(duration: 1.0)) {
-                        punchlineOpacity = 1.0
-                    }
-                }, label:{
-                    Image(systemName:"arrow.down.circle.fill")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 40)
-                        .tint(.black)
-                })
+                if let currentJoke = currentJoke {
+                    
+                    Text(currentJoke.setup)
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                    
+                    Button(action: {
+                        withAnimation(.easeIn(duration: 1.0)) {
+                            punchlineOpacity = 1.0
+                        }
+                    }, label:{
+                        Image(systemName:"arrow.down.circle.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 40)
+                            .tint(.black)
+                    })
+                    
+                    Text(currentJoke.punchline)
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                        .opacity(punchlineOpacity)
+                    
+                    
+                    // Show the joke, if it can be unwrapped (if currentJoke is not nil)
+                } else {
+                    // Show a spinning wheel indicator untill the joke is loaded
+                    ProgressView()
+                }
+                    
+                // Create an asynchronous task to be performed as this view appears
                 
-                Text(currentJoke.punchline)
-                    .font(.title)
-                    .multilineTextAlignment(.center)
-                    .opacity(punchlineOpacity)
             }
+            .task {
+                currentJoke = await NetworkService.fetch()
+            }
+            .navigationTitle("Random Jokes")
         }
-        .navigationTitle("Random Jokes")
+        
     }
 }
+    struct JokeView_Previews: PreviewProvider {
+        static var previews: some View {
+            JokeView()
+        }
+    }
 
-struct JokeView_Previews: PreviewProvider {
-    static var previews: some View {
-        JokeView()
-    }
-}
