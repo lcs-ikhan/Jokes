95fb13c7e7eeb2a4fe13da9cc88f659149556f25
Isaad Khan <IKhan@lcs.on.ca>
2023-04-18 18:20:58 -0400

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Isaad Khan on 2023-04-18.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
