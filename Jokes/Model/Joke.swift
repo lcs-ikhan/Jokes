5ccfc7071cd3af848a04e36079ba7cd0d6d74553
Isaad Khan <IKhan@lcs.on.ca>
2023-04-20 08:55:38 -0400

    Adjusted the model so that a joke can be read from or written to the database.

--- Jokes/Model/Joke.swift
+++ Jokes/Model/Joke.swift
@@ -5,13 +5,14 @@
 //  Created by Isaad Khan on 2023-04-18.
 //
 
+import Blackbird
 import Foundation
 
-struct Joke: Identifiable, Codable {
-    let type: String
-    let setup: String
-    let punchline: String
-    let id: Int
+struct Joke: Identifiable, Codable, BlackbirdModel{
+    @BlackbirdColumn var type: String
+    @BlackbirdColumn var setup: String
+    @BlackbirdColumn var punchline: String
+    @BlackbirdColumn var id: Int
 }
 
 let exampleJoke = Joke(type: "general",
